812d8bba4b794c727e7a04ba7d3211fca44538ce
Mashhood Qadeer <mas12qa387@gmail.com>
2024-12-07 18:56:13 +0500

    Initial files have been added

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Mashhood Qadeer on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
